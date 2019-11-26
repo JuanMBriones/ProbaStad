c83b941ec1c9671c1d618e91c83f028627a6b275
Gerardo Granados Aldaz <gerardogr.al@outlook.com>
2019-11-26 03:04:31 -0700

    Made pretty

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
 
+import 'package:rflutter_alert/rflutter_alert.dart';
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -66,9 +68,7 @@ class _MyHomePageState extends State<MyHomePage> {
                     child: RaisedButton(
                       padding: EdgeInsets.symmetric(vertical: 50),
                       child: Text("Analyze Data"),
-                      onPressed: () {
-                        DataSet data = new DataSet(DataInput.dataPoints);
-                      },
+                      onPressed: () => goToAnalyze(),
                     ),
                   ),
                 ),
@@ -84,10 +84,45 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
               ],
             ),
+            Container(
+              margin: new EdgeInsets.only(top: 300.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Expanded(
+                    child: new Container(
+                      margin: EdgeInsets.all(5),
+                      child: RaisedButton(
+                        padding: EdgeInsets.symmetric(vertical: 40),
+                        child: Text("Delete Data Set"),
+                        onPressed: () {
+                          DataInput.dataPoints.clear();
+                        },
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  void goToAnalyze() {
+    if (DataInput.dataPoints.length > 0) {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => AnalyzeDataRoute()),
+      );
+    } else {
+      Alert(
+              context: context,
+              title: "Data Set Empty",
+              desc: "Please input a Data Set in the \"Input Data Set\" section")
+          .show();
+    }
   }
 }
 
@@ -107,49 +142,119 @@ class DataInput extends State<InputDataRoute> {
       appBar: AppBar(
         title: Text("Input Data Set"),
       ),
-      body: new Column(
-        children: <Widget>[
-          new TextField(
-            controller: eCtrl,
-            keyboardType: TextInputType.number,
-            onSubmitted: (text) {
-              double dPoint = double.parse(text);
-              dataPoints.add(dPoint);
-              eCtrl.clear();
-              setState(() {});
-            },
-          ),
-          new Expanded(
-            child: new ListView.builder(
-              itemCount: dataPoints.length,
-              itemBuilder: (BuildContext ctxt, int Index) {
-                return new Text(dataPoints[Index].toString());
-              },
+      body: new Container(
+        padding: new EdgeInsets.only(left: 20.0, right: 20.0, top: 20.0),
+        child: Column(
+          children: <Widget>[
+            Container(
+              margin: EdgeInsets.only(bottom: 15),
+              child: new TextField(
+                decoration: InputDecoration(hintText: "Insert data point here"),
+                controller: eCtrl,
+                keyboardType: TextInputType.number,
+                onSubmitted: (text) {
+                  double dPoint = double.parse(text);
+                  dataPoints.add(dPoint);
+                  eCtrl.clear();
+                  setState(() {});
+                },
+              ),
+              decoration: BoxDecoration(
+                  border: new Border.all(
+                color: Colors.purple,
+                width: 3,
+              )),
             ),
-          )
-        ],
+            new Expanded(
+              child: new ListView.builder(
+                itemCount: dataPoints.length,
+                itemBuilder: (BuildContext ctxt, int Index) {
+                  return new Dismissible(
+                    child: Container(
+                      padding: new EdgeInsets.symmetric(vertical: 0.0),
+                      decoration: BoxDecoration(
+                        border: new Border(
+                          bottom: new BorderSide(
+                              color: Colors.black,
+                              width: 1.5,
+                              style: BorderStyle.solid),
+                        ),
+                      ),
+                      child: ListTile(
+                          title: new Text(dataPoints[Index].toString()),
+                          trailing: new Icon(Icons.delete_sweep)),
+                    ),
+                    key: new Key(dataPoints[Index].toString()),
+                    onDismissed: (direction) {
+                      dataPoints.removeAt(Index);
+                    },
+                    background: new Container(
+                      color: Colors.red,
+                      child: Icon(
+                        Icons.delete,
+                        color: Colors.white,
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+//Analyze Data ↓
+class AnalyzeDataRoute extends StatefulWidget {
+  @override
+  State createState() => new DataAnalysis();
+}
+
+class DataAnalysis extends State<AnalyzeDataRoute> {
+  // @override
+  DataSet myData = new DataSet();
+
+  Widget build(BuildContext ctxt) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Analyze Data"),
+      ),
+      body: new Container(
+        padding: EdgeInsets.only(left: 20.0, right: 20.0, top: 20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[Column()],
+            ),
+          ],
+        ),
       ),
     );
   }
 }
 
 class DataSet {
-  static List<double> data;
-  static var mean;
-  static var median;
-  static List<double> modes;
-  static double range;
-  static double variance;
-  static double stdDeviation;
+  List<double> data = DataInput.dataPoints;
+  double mean;
+  double median;
+  List<double> modes;
+  double range;
+  double variance;
+  double stdDeviation;
 
-  DataSet(List<double> dataPoints) {
-    data = dataPoints;
-    data.sort();
+  DataSet() {
     getStats(data);
+    // printThings();
   }
 
-  void getStats(List<double> data) {
-    mean = data.reduce((a, b) => a + b) / data.length;
+  void getStats(List<double> dataPoints) {
+    data = dataPoints;
+    data.sort();
+    mean = getMean(data);
     median = getMedian(data);
     modes = getModes(data);
     range = data[data.length - 1] - data[0];
@@ -157,7 +262,22 @@ class DataSet {
     stdDeviation = sqrt(variance);
   }
 
+  double getMean(data) {
+    if (data.length == 1) {
+      return data[0];
+    }
+    double sum = 0;
+    for (int i = 0; i < data.length; ++i) {
+      sum += data[i];
+    }
+
+    return sum / data.length;
+  }
+
   double getMedian(data) {
+    if (data.length == 1) {
+      return data[0];
+    }
     double half = data.length / 2;
     int med = half.round();
     if (data.length % 2 == 1) {
@@ -169,6 +289,10 @@ class DataSet {
 
   List<double> getModes(data) {
     List<double> modes = [];
+    if (data.length == 1) {
+      modes.add(data[0]);
+      return modes;
+    }
     List<double> counted = [];
     int maxCount = 0;
 
@@ -200,4 +324,11 @@ class DataSet {
     }
     return sum / data.length;
   }
+
+  void printThings() {
+    print(
+        "mean: $mean\nmedian: $median\nmodes: $modes\nordered list: $data\nvariance: $variance\nstd deviation: $stdDeviation");
+  }
 }
+
+class Grapher {}
